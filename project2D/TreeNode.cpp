f4aeda1c4b31a9c50175621bb7225b184d15b7ca
ThomasLKing117 <king.thomas.l.117@gmail.com>
2018-11-07 12:58:46 -0600

    started binary

---
+++ project2D/TreeNode.cpp
@@ -0,0 +1,26 @@
+#include "TreeNode.h"
+#include <iostream>
+
+extern aie::Font* g_systemFont;
+
+void TreeNode::draw(aie::Renderer2D * renderer, int x, int y, bool selected)
+{
+	static char buffer[10];
+
+	sprintf(buffer, "%d", m_value);
+
+	renderer->setRenderColour(.22f, 1.f, .08f);
+	renderer->drawCircle(x, y, 30);
+
+	if (selected == true)
+	{
+		renderer->setRenderColour(.5f, .5f, 0);
+	}
+	else
+	{
+		renderer->setRenderColour(0, 0, 0);
+		renderer->drawCircle(x, y, 28);
+		renderer->setRenderColour(1, 1, 1);
+		renderer->drawText(g_systemFont, buffer, x - 12, y - 10);
+	}
+}
